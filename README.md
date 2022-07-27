37a0de4f81c6e050a5c867c4e3e26702c3729819
Moi6-23 <77814787+Moi6-23@users.noreply.github.com>
2022-07-27 16:35:15 -0500

    Readme
    Information challenge

--- README.md
+++ README.md
@@ -1,24 +1,45 @@
-# biz-app
+# ESCENARIO
+### Crear una aplicación web que realice las siguientes funciones:
+1.	La primera parte consta de consultar datos de una API acerca de películas dependiendo de la categoría. La aplicación debe ser capaz de mostrar los datos de forma visual como son: la imagen, el nombre y la fecha (la cantidad de películas que consulta debe ser modificable). A su vez, los datos deben ser almacenados de forma local de modo que al recargar la página no se pierda la información. Una vez se tiene los datos de forma local el usuario tendrá dos opciones, la primera de eliminar algún dato y la segunda de indicar una insignia como favorito. Por último, al hacer click en la imagen de una película se debe redirigir a otra vista encargada de presentar la descripción de la película con los elementos que la componen. 
 
-## Project setup
+2.	En la segunda parte es necesario crear una vista en donde el usuario pueda adicionar información que no se encuentre en la Api, es decir, la posibilidad de agregar una nueva película compuesta por el título, imagen, descripción y fecha. Esta a su vez se presentará en el Home con los demás datos en caso de ser consultados.
+
+3.	[Opcional] Desarrollar una vista en donde se puedan observar las películas seleccionadas como favoritas, en todos los casos la información al recargar la página debe persistir.
+
+4.	[Opcional] Agregar una vista en donde sea posible filtrar los datos consultados.
+
+### Requerimientos del proyecto:
+-	Use cualquier lenguaje o framework (Vue JS, React JS, Angular JS, Javascript, Go, NodeJs, ...)
+-	Implementar el control de rutas por medio de un enrutador.
+-	Desarrollar un NavBar para la navegación entre vistas.
+-	El rango de los datos a consultar debe ser modificable.
+-	Implementar y consumir el estado de la aplicación por medio de un gestor de estado como Vuex, Context, Redux, Redux toolkit, NGXS. 
+-	Guarde los datos mediante LocalStore.
+-	Consulta de API web.
+-	Puede crear la aplicación para que se ejecute localmente o en una nube pública (Azure, AWS, GCP) utilizando cualquier pila de tecnología disponible para usted en ese entorno.
+-	La interfaz de usuario es opcional.
+-	Debe mantener su código en un repositorio de control de código fuente (GitHub, Bitbucket, etc.).
+
+# Desarrollo del reto biz-app
+![Home2](https://i.ibb.co/Hx8Vn1C/Home.png)
+### Configuración para ejecutar el proyecto de forma local:
 ```
 npm install
 ```
 
-### Compiles and hot-reloads for development
+### Ejecutar servidor de forma local
 ```
 npm run serve
 ```
 
-### Compiles and minifies for production
-```
-npm run build
-```
+## La aplicación web se encuentra alojada en vercel   ->  [biz-app](https://biz-app.vercel.app/) 
+![Home](https://i.ibb.co/yyW3ZWR/Home2.png)
+## Features
 
-### Run your unit tests
-```
-npm run test:unit
-```
+- Busqueda de películas relacionadas dependendiendo de la categoría.
+- Buscador para filtrar las películas almacenadas.
+- Posibilidad para agregar a favoritos y ser consultados en otra vista.
+- Vista para adiccionar películas de forma local.
+- Todos los datos son guardados en localStore.
+- La aplicación web es responsiva.
 
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/views/FilterMovieView.vue
+++ src/views/FilterMovieView.vue
@@ -1,45 +1,55 @@
 <template>
-  <div class="container col-auto mt-3 ">
-      <div>
+  <div class="container col-auto mt-3">
+    <div>
       <div class="row">
         <div class="col-12 col-sm-5 mb-4">
           <h4>Search Movie</h4>
           <hr />
           <form @submit.prevent="onSubmit">
-            <div class="input-group mb-3 ">
+            <div class="input-group mb-3">
               <input
                 type="text"
                 class="form-control"
                 placeholder="Title movie"
                 aria-describedby="button-addon2"
-                autoComplete='off'
-                v-model='searchInput'
+                autoComplete="off"
+                v-model="searchInput"
               />
-              <button class="btn btn-primary ps-4 pe-4" @click="dataFilterClick">  Filter  </button>
+              <button
+                class="btn btn-primary ps-4 pe-4"
+                @click="dataFilterClick"
+              >
+                Filter
+              </button>
             </div>
           </form>
         </div>
-        <div class='col-12 col-sm-7'>
+        <div class="col-12 col-sm-7">
           <h4>Results</h4>
-          <hr/>
-          <div >
-              <div class='card mb-3' v-for="dataFilter in dataFilters" :key="dataFilter.id">
-                  <div class='row no-gutters'>
-                      <div class='col-md-4'>
-                          <img
-                              class='card-img'
-                              :src=dataFilter.imgPath
-                          />
-                      </div>
-                      <div class='col-8'>
-                          <div class='card-body'>
-                              <h5 class='card-title'>{{dataFilter.title}}</h5>
-                              <p class='card-title'>{{dataFilter.release_date}}</p>
-                              <router-link to='/home/screen' @click="screenMovie(dataFilter)">More...</router-link>
-                          </div>
-                      </div>
+          <hr />
+          <div>
+            <div
+              class="card mb-3"
+              v-for="dataFilter in dataFilters"
+              :key="dataFilter.id"
+            >
+              <div class="row no-gutters">
+                <div class="col-md-4">
+                  <img class="card-img" :src="dataFilter.imgPath" />
+                </div>
+                <div class="col-8">
+                  <div class="card-body">
+                    <h5 class="card-title">{{ dataFilter.title }}</h5>
+                    <p class="card-title">{{ dataFilter.release_date }}</p>
+                    <router-link
+                      to="/home/screen"
+                      @click="screenMovie(dataFilter)"
+                      >More...</router-link
+                    >
                   </div>
+                </div>
               </div>
+            </div>
           </div>
         </div>
       </div>
@@ -48,26 +58,28 @@
 </template>
 
 <script>
-import {ref} from 'vue';
-import { useStore } from 'vuex';
+import { ref } from "vue";
+import { useStore } from "vuex";
 
 export default {
-    name:'FilterMovie',
-    setup(){
-        const searchInput = ref('');
-        const dataFilters = ref([]);
-        const store = useStore();
-        const filterMovie = JSON.parse(localStorage.getItem('moviesData'));
-        const dataFilterClick = () => {
-            let titleFilter = searchInput.value.toLowerCase();
-            dataFilters.value = filterMovie.filter((el) => el.title.toLowerCase().includes(titleFilter));
-        }
-        const screenMovie = (movie) => {
-          store.commit('screenMovie', movie)
-        }
-        return {searchInput, dataFilters, dataFilterClick, screenMovie}
-    }
-}
+  name: "FilterMovie",
+  setup() {
+    const searchInput = ref("");
+    const dataFilters = ref([]);
+    const store = useStore();
+    const filterMovie = JSON.parse(localStorage.getItem("moviesData"));
+    const dataFilterClick = () => {
+      let titleFilter = searchInput.value.toLowerCase();
+      dataFilters.value = filterMovie.filter((el) =>
+        el.title.toLowerCase().includes(titleFilter)
+      );
+    };
+    const screenMovie = (movie) => {
+      store.commit("screenMovie", movie);
+    };
+    return { searchInput, dataFilters, dataFilterClick, screenMovie };
+  },
+};
 </script>
 
 <style>
