351d2a5fb95798bc4bf04ec311e928f5ed688878
Moi6-23 <77814787+Moi6-23@users.noreply.github.com>
2022-07-27 13:14:05 -0500

    FavoritesView, MovieCard
    Development of the view favorites adding the template similar to the one of MovieList with some changes. A component called MovieCard is created which is the one iterated over in FavoritesView.

--- src/views/FavoritesView.vue
+++ src/views/FavoritesView.vue
@@ -1,11 +1,41 @@
-<template>
-  <div >
-    Hola Favorites
-  </div>
-</template>
-
 <script>
+import { mapState } from 'vuex'
+import MovieCard from '../components/Movie/MovieCard.vue';
+
 export default {
   name: 'FavoritesView',
+  computed: mapState({
+    movieFavorites: state => state.favorites
+  }),
+  components:{
+    MovieCard
+  },
+  methods:{
+    deleteFavorites(movie){
+      this.$store.commit('deleteFavorites', movie)
+    },
+    screenMovie(movie){
+      this.$store.commit('screenMovie', movie)
+    },
+  }
 }
 </script>
+
+<template>
+  <section class="container">
+    <div class="row row-cols-1 row-cols-sm-3 g-3 mt-2">
+      <div
+        class="col animate__animated animate__backInLeft"
+        v-for="movie in movieFavorites"
+        :key="movie.id"
+      >
+        <MovieCard
+          :movie="movie"
+        />
+      </div>
+    </div>
+  </section>
+</template>
+
+<style>
+</style>
